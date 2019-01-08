52e74833c20a93a036764a8ddb06b8a568675102
vladbulanyy <coollife@i.ua>
2019-01-08 23:17:00 +0200

    added core project

---
+++ Core/BloodDonor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Core
+{
+    public class BloodDonor
+    {
+        public string BloodDonorID { get; set; }
+        public string Name { get; set; }
+        public DateTime Dob { get; set; }
+        public string BloodGroup { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int PinCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsPrivate { get; set; }
+        public bool IsVerified { get; set; }
+    }
+}
---
+++ Core/Interfaces/IBloodDonorRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Interfaces
+{
+    interface IBloodDonorRepository
+    {
+        //1-3445672832 : Complain numbert Tikona 
+        void Add(BloodDonor b);
+        void Edit(BloodDonor b);
+        void Remove(string BloodDonorID);
+        IEnumerable<BloodDonor> GetBloodDonors();
+        BloodDonor FindById(string BloodDonorID);
+    }
+}
